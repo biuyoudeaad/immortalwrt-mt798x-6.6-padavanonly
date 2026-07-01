6a4f78cf5793ef384de46a68ce4640a8a8a9d8f1
boboj <boboj@gmail.com>
2026-07-01 23:35:41 +0800

    update pgy device name
    Signed-off-by: boboj <boboj@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -936,7 +936,7 @@ endef
 TARGET_DEVICES += cudy_tr3000-v1-256mb
 
 define Device/pgy_x1pro-v1-ubootmod
-  DEVICE_VENDOR := Pgy
+  DEVICE_VENDOR := PGY
   DEVICE_MODEL := X1Pro
   DEVICE_VARIANT := v1 MOD
   DEVICE_DTS := mt7981b-pgy-x1pro-v1-ubootmod
